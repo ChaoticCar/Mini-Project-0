d4b9d89a1de1b7abc90fd2c3c536d0120922dff7
ChaoticCar14 <ChaoticCar14@gmail.com>
2026-01-29 18:02:20 -0500

    Code

---
+++ Objects/main.d
@@ -0,0 +1,5 @@
+.\objects\main.o: Code\Main.c
+.\objects\main.o: C:\Users\cjjen\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdio.h
+.\objects\main.o: C:\Users\cjjen\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+.\objects\main.o: Code\PLL.h
+.\objects\main.o: Code\UART.h
---
+++ Objects/pll.d
@@ -0,0 +1,4 @@
+.\objects\pll.o: Code\PLL.c
+.\objects\pll.o: C:\Users\cjjen\AppData\Local\Keil_v5\ARM\ARM_Compiler_5.06u7\Bin\..\include\stdint.h
+.\objects\pll.o: Code\PLL.h
+.\objects\pll.o: Code\tm4c123gh6pm.h
